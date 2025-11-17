2cb36ba522a5c155689d745b5561ac0ff21481d2
Tayps <47232291+Tayps@users.noreply.github.com>
2025-11-18 00:29:03 +0800

    create main() function

--- main.py
+++ main.py
@@ -45,13 +45,13 @@ conn = psycopg2.connect(host = os.getenv("PG_HOST"),
                         port = os.getenv("PG_PORT"))
 cur = conn.cursor()
 
-# Retrieve last draw recorded in DB.
+# Retrieve last draw recorded in DB. Returns an integer
 def getDB():
     cur.execute("SELECT MAX(drawnumber) FROM toto")
     drawDB = cur.fetchall()[0][0]
     return drawDB
 
-# Retrieve last draw from singapore pools
+# Retrieve last draw from singapore pools, returns an integer
 def getSP():
     driver.get("https://www.singaporepools.com.sg/en/product/sr/pages/toto_results.aspx")
     driver.set_window_size(1024,1024)
@@ -59,6 +59,14 @@ def getSP():
     drawSP = int(drawSP.text.replace('Draw No. ', ''))
     return drawSP
 
+#Compare latest draw in DB with Singapore Pools, return Bool.
+def dataLag():
+    SP = getSP()
+    DB = getDB()
+    datalag = (SP > DB) #mismatch if SP data is more than DB data
+    return datalag 
+
+# Retrieve draw data from SP, returns a dictionary.
 def dataRetrieve():
     drawData = {}
     drawData["drawNumber"] = int(driver.find_element(By.XPATH, "/html/body/form/div[5]/div[1]/div/section/div/div/div/span/section/div/div[2]/div[2]/div/div/div[2]/div[1]/div/div[1]/div/div[2]/div[1]/table[1]/thead/tr/th[2]").text.replace('Draw No. ', ''))
@@ -73,12 +81,11 @@ def dataRetrieve():
         drawData[f"g{row-1}WinShares"] = int(driver.find_element(By.XPATH, "/html/body/form/div[5]/div[1]/div/section/div/div/div/span/section/div/div[2]/div[2]/div/div/div[2]/div[1]/div/div[1]/div/div[2]/div[1]/table[5]/tbody/tr["+str(row)+"]/td[3]").text.replace(',','').replace('-','0'))
     return drawData
 
-def main():
-    # dbconnect()
-    SP = getSP()
-    DB = getDB()
-    if SP > DB:
-        missing = SP - DB
+#  Retrieval of data
+def scrapeData():
+    if dataLag():
+        DB = getDB()
+        missing = getSP() - DB
         for draw in range(1, missing +1):
             options = Select(driver.find_element(By.XPATH, "/html/body/form/div[5]/div[1]/div/section/div/div/div/span/section/div/div[2]/div[2]/div/div/div[2]/div[1]/div/div[1]/div/div[1]/div[2]/div/select"))
             options.select_by_value(f"{DB + draw}")
@@ -87,10 +94,9 @@ def main():
             cur.execute(dataInsert, insertVal)
             conn.commit()
 
+def main():
+    # dbconnect()
+    scrapeData()
+
 if __name__ == '__main__':
     main()
-
-
-        
-
-    
